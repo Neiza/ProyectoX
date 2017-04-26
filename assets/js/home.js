3ab3396ec0e8e43aacb497d1b7705cb4fe904ee6
GreeceArtemis <greece.rapu@gmail.com>
2017-04-26 07:56:14 -0500

    div movies y series terminado

--- assets/js/home.js
+++ assets/js/home.js
@@ -49,29 +49,75 @@ function showNext1() {
    if (index > mySlides.length) {
      index = 1;
    }
-   console.log(index);
    mySlides[index-1].style.display = "block";
  });
 
+var listMovies=20;
+var listSeries=10;
+var nameFileMovies="assets/img/img-home/p";
+var nameFileSeries="assets/img/img-home/serie";
 
 //
-function divImage(listMovies,nameFile){
-      var contForDiv=document.getElementById("cont-list-movies");
-      for(var i=0;i<listMovies.length;i++){
-          var divForMovie=document.createElement("div");
+function divImage(list,nameFile,slides,nameDiv,type,nameimg){
+      var contForDiv=document.getElementById(slides);
+      var a=0;
+      for (var j=0;(j<list);j+=2){
+          var divFortwo=document.createElement("div");
+          divFortwo.setAttribute("id",nameDiv+"two"+(j+1));
+          divFortwo.setAttribute("class",nameDiv+"two");
 
-          contForDiv.appendChild(divForMovie);
-          divForMovie.setAttribute("id",name);
-          divForMovie.setAttribute("class","divCoder"+nameFile);
-          var imgForMovie=document.createElement("img");
-          imgForMovie.setAttribute("src","assets/image/"+nameFile+"/"+listMovies[i]);
-          imgForMovie.setAttribute("class",nameFile);
-          divForMovie.appendChild(imgForMovie);
-          var space=document.createElement("br");
-          divForMovie.appendChild(space);
-          var nameCoder=document.createElement("span");
-          nameCoder.classList.add("nameCoder");
-          nameCoder.innerHTML=name;
-          divForMovie.appendChild(nameCoder);
+        for(var i=1;i<3;i++){
+            var divForElement=document.createElement("div");
+            divForElement.setAttribute("id","div"+type+(i+j));
+            divForElement.setAttribute("class","div"+type);
+            var imgForElement=document.createElement("img");
+            imgForElement.setAttribute("src",nameFile+(i+j)+".jpg");
+            imgForElement.setAttribute("class",nameDiv);
+            divForElement.appendChild(imgForElement);
+            var nameElement=document.createElement("span");
+            nameElement.classList.add(type+"S"+(i+1));
+
+            divForElement.appendChild(nameElement);
+            divFortwo.appendChild(divForElement);
+        }
+
+        contForDiv.appendChild(divFortwo);
       }
+
+
   }
+divImage(listMovies,nameFileMovies,"slidesMovies","divMovie","Movie","p");
+divImage(listSeries,nameFileSeries,"slidesSeries","divSerie","Serie","serie");
+var indexListM = 1;
+var indexListS=1;
+var t=5000;
+var btnAfterMovie=document.getElementById("btnAfterMovie");
+var btnBeforeMovie=document.getElementById("btnBeforeMovie");
+var mySlidesMovie = document.getElementsByClassName("divMovieTwo");
+btnBeforeMovie.addEventListener("click", beforeDiv(indexListM,mySlidesMovie));
+btnAfterMovie.addEventListener("click",afterDiv(indexListM,mySlidesMovie) );
+function beforeDiv(indexX,mySlidesX){
+  for (i = 0; i < mySlides.length; i++) {
+     mySlidesX[i].style.display = "none";
+  }
+    indexX=indexX-1;
+    if(indexX>0){
+      console.log(indexX);
+      mySlidesX[indexX-1].style.display = "block";
+    }
+    else{
+      indexX=mySlidesX.length;
+       mySlidesX[indexX-1].style.display = "block";
+    }
+}
+function afterDiv(){
+  indexX++;
+
+  for (i = 0; i < mySlidesX.length; i++) {
+     mySlidesX[i].style.display = "none";
+  }
+  if (indexX > mySlidesX.length) {
+    indexX = 1;
+  }
+  mySlidesX[indexX-1].style.display = "block";
+}
